9256a449f63e7c1bad921d0f8832bc277e4f8c74
AyeshaNaime003 <ayeshashamim003@gmail.com>
2023-12-09 22:37:32 +0500

    app/url, views, page creations

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ django-admin startapp app
 
 confugure the app in the settings
  "app.apps.AppConfig"
+
+ make template dir alongside app and server
+ configure so the server can see the template:   BASE_DIR/'templates' in setting.py
+
+ pages:
+ login,
+ home,
+ get sentinel2 imagery form,
+ stats,
+ map
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("this is Home page from views")
+    return render(request, "app/home.html")
+def about(request):
+    return render(request, "app/about.html")
