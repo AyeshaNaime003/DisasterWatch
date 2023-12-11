08f4f1720abc002ce41fa605a06f07fa1b7b65de
AyeshaNaime003 <ayeshashamim003@gmail.com>
2023-12-11 22:40:08 +0500

    news api integration

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+import requests
 
 
 
 # Create your views here.
+@login_required(login_url="login/")
 def home(request):
-    return render(request, "app/home.html")
+    api_url = "https://api.reliefweb.int/v1/reports?appname=apidoc&preset=latest&query[value]=earthquake&limit=5"
+    response = requests.get(api_url)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        api_data = response.json()        
+        reports = api_data.get("data", [])    
+        print(reports)    
+        return render(request, 'app/home.html', {'reports': reports})
+    else:
+        # Handle the error gracefully
+        error_message = f"Failed to fetch data from API. Status code: {response.status_code}"
+        return render(request, 'app/home.html', {'error_message': error_message})
+
 def about(request):
     return render(request, "app/about.html")
+
 def loginPage(request):
     if request.method=="POST":
         username = request.POST.get('username')
@@ -26,3 +43,7 @@ def loginPage(request):
         else:
             messages.error(request, "Invalid credentials")
     return render(request, "app/login.html")
+
+def logoutPage(request):
+    logout(request)
+    return redirect("login")
