61d9474b8fbc6e92a56a66f64645fcafc207cb64
AyeshaNaime003 <ayeshashamim003@gmail.com>
2024-03-03 03:23:16 +0500

    logout, urls for sidebar pages

--- app/urls.py
+++ app/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutPage, name="logout"),
     path('map/', views.map, name="map"),
     path('admin-panel/', views.adminPanel, name="admin-panel"),
     path('dashboard/', views.dashboard, name="dashboard"),
+    path('profile/', views.profile, name="profile"),
+    path('notifications/', views.notifications, name="notifications"),
+    path('help/', views.help, name="help"),
+    path('settings/', views.settings, name="settings"),
 ]
--- app/views.py
+++ app/views.py
@@ -10,27 +10,6 @@ from .folium_maps import html_code
 from .map_segmentation import html_code
 # from .plotly_maps import dash_app
 
-
-# Create your views here.
-@login_required(login_url="login/")
-def home(request):
-    api_url = "https://api.reliefweb.int/v1/reports?appname=apidoc&preset=latest&query[value]=earthquake&limit=6"
-    response = requests.get(api_url)
-    
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        api_data = response.json()        
-        reports = api_data.get("data", [])    
-        print(reports)    
-        return render(request, 'app/home.html', {'reports': reports})
-    else:
-        # Handle the error gracefully
-        error_message = f"Failed to fetch data from API. Status code: {response.status_code}"
-        return render(request, 'app/home.html', {'error_message': error_message})
-
-def about(request):
-    return render(request, "app/about.html")
-
 def loginPage(request):
     if request.method=="POST":
         username = request.POST.get('username')
@@ -39,14 +18,37 @@ def loginPage(request):
             user = User.objects.get(username=username)
         except:
             messages.error(request, "No user with this username")
-
         user = authenticate(request=request, username=username, password=password)
         if user is not None:
             login(request, user)
+            print("LOGIN SUCCESSFUL, REDIRECTING TO HOME")
             return redirect('home')
         else:
             messages.error(request, "Invalid credentials")
     return render(request, "app/login.html")
+
+# Create your views here.
+@login_required(login_url="login/")
+def home(request):
+    print(f"HII {request.user.username}, welcome to home page")
+    api_url = "https://api.reliefweb.int/v1/reports?appname=apidoc&preset=latest&query[value]=earthquake&limit=6"
+    response = requests.get(api_url)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        api_data = response.json()        
+        reports = api_data.get("data", [])    
+        # for key
+        return render(request, 'app/home.html', {'reports': reports})
+    else:
+        # Handle the error gracefully
+        error_message = f"Failed to fetch data from API. Status code: {response.status_code}"
+        return render(request, 'app/home.html', {'error_message': error_message})
+
+
+
+def about(request):
+    return render(request, "app/about.html")
 
 @login_required(login_url="login/")
 def logoutPage(request):
@@ -61,3 +63,15 @@ def adminPanel(request):
 
 def dashboard(request):
     return render(request, "app/dashboard.html")
+
+def profile(request):
+    return render(request, "app/profile.html")
+
+def help(request):
+    return render(request, "app/help.html")
+
+def notifications(request):
+    return render(request, "app/notifications.html")
+
+def settings(request):
+    return render(request, "app/settings.html")
